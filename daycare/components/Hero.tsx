29a60666c881ea47254e046143df34efe05f6886
kooya3 <elyeestatua@gmail.com>
2023-12-01 01:44:44 +0300

    ops(Hero): Working on the Hero section location section

--- daycare/components/Hero.tsx
+++ daycare/components/Hero.tsx
@@ -56,24 +56,24 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+        <div className="relative z-20 flex w-[270px] flex-col gap-8 rounded-3xl bg-yellow-50 px-7 py-8">
 
            <div className="flex flex-col">
             <div className="flexBetween">
-              <p className="regular-16 text-gray-20">Location</p>
+              <p className="regular-16 text-green-70">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-yellow-50">Kiambu, Ruaka.</p>
+            <p className="bold-20 text-black">Kiambu, Ruaka.</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="regular-16 block text-green-70">Distance</p>
+              <p className="bold-20 text-black">173.28 mi</p>
             </div>
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Elevation</p>
-              <p className="bold-20 text-white">13.040 km</p>
+              <p className="regular-16 block text-green-70">Elevation</p>
+              <p className="bold-20 text-black">13.040 km</p>
             </div>
           </div>
         </div>
