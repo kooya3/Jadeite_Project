9cb8403f68ccfbc653a2730b6491079a2959390c
kooya3 <elyeestatua@gmail.com>
2023-12-04 22:18:18 +0300

    ops(Footer): Working on something for the description

--- daycare/components/Footer.tsx
+++ daycare/components/Footer.tsx
@@ -8,9 +8,9 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href="/" className="mb-10">
+          {/* <Link href="/" className="mb-10">
             <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
-          </Link>
+          </Link> */}
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
             {FOOTER_LINKS.map((columns) => (
@@ -49,7 +49,7 @@ const Footer = () => {
                 <ul className="regular-14 flex gap-4 text-gray-30">
                   {SOCIALS.links.map((link) => (
                     <Link href="/" key={link}>
-                      
+                      <Image src={link} alt="logo" width={24} height={24} />
                     </Link>
                   ))}
                 </ul>
