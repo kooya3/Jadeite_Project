d951fc14653d1c03d352acb509c4d8b81b60d5b4
kooya3 <elyeestatua@gmail.com>
2023-12-05 20:07:09 +0300

    ops(Finish): Setting up donate functionality later

--- daycare/components/Button.tsx
+++ daycare/components/Button.tsx
@@ -1,24 +1,23 @@
-// Button.tsx
 import Image from "next/image";
-import Link from "next/link";
 
 type ButtonProps = {
+  type: 'button' | 'submit';
   title: string;
   icon?: string;
   variant: string;
   full?: boolean;
-  href?: string; // Add href prop for client-side navigation
-};
+}
 
-const Button = ({ title, icon, variant, full, href }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
-    <Link href={href || '/'} passHref>
-      <a className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
-        {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
-      </a>
-    </Link>
-  );
-};
+    <button
+    className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+    </button>
+  )
+}
 
-export default Button;
+export default Button
