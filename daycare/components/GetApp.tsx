67cfbf8edc7e8aa0ae1765d75e9c89c829edb5e7
kooya3 <elyeestatua@gmail.com>
2023-12-06 17:07:01 +0300

    build(Finished): Done with the webpage

--- daycare/components/GetApp.tsx
+++ daycare/components/GetApp.tsx
@@ -7,23 +7,29 @@ const GetApp = () => {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Write US!</h2>
-          <p className="regular-16 text-gray-10">A message from you is priceless.</p>
+        <a href="https://www.mchanga.africa/fundraiser/85304" target="_blank" rel="noopener noreferrer">
+        <h2 className="bold-40 lg:bold-40 xl:max-w-[320px] text-yellow-50">Donate Here</h2>
+        </a>
+          <h2 className="bold-32 lg:bold-40 xl:max-w-[320px]">PAYBILL: 891300</h2>
+          <h2 className="bold-32 lg:bold-40 xl:max-w-[320px]">ACCOUNT: 85304</h2>
+          <p className="regular-16 text-gray-10">DIAL: *483*57*85304#</p>
+
+
           <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
-            <Button 
+            {/*  <Button
               type="button"
-              title="App Store"
+              title="Donate"
               icon="/apple.svg"
               variant="btn_white"
               full
             />
-            <Button 
+            <Button
               type="button"
               title="Play Store"
               icon="/android.svg"
               variant="btn_dark_green_outline"
               full
-            />
+            /> */}
           </div>
         </div>
 
--- daycare/components/Navbar.tsx
+++ daycare/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        <Image src="/logo.png" alt="logo" width={74} height={29} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
@@ -19,21 +19,21 @@ const Navbar = () => {
       </ul>
 
       <div className="lg:flexCenter hidden">
-        <Button 
+        <a href="https://www.mchanga.africa/fundraiser/85304" target="_blank" rel="noopener noreferrer"><Button 
           type="button"
-          title="Login"
+          title="Donate"
           icon="/user.svg"
           variant="btn_dark_green"
-        />
+        /></a>
       </div>
-
+{/* 
       <Image 
         src="menu.svg"
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
-      />
+      /> */}
     </nav>
   )
 }
