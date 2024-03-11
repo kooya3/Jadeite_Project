3f3ccac0a587902e3af2d9bb084262ebba1491e7
Elyees Tatua <elyeestatua@gmail.com>
2024-03-11 15:35:26 +0300

    chore(page): Updating page to fix error

--- daycare/components/Camp.tsx
+++ daycare/components/Camp.tsx
@@ -28,20 +28,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
       </div>
 
       <div className="flexCenter gap-6">
-        <span className="flex -space-x-4 overflow-hidden">
-          {PEOPLE_URL.map((url) => (
-            <Image 
-              className="inline-block h-10 w-10 rounded-full"
-              src={url}
-              key={url}
-              alt="person"
-              width={52}
-              height={52}
-              layout="responsive"
-            />
-          ))}
-        </span>
-        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+      <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
       </div>
      </div>
     </div>
